d6d907db8313ba47f96c37c812120f1c6b15ab27
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-08-09 09:28:15 +0900

    Title: 알파벳에 따른 평가, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240809/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+scr = input()
+if scr =='S':
+    print("Superior")
+elif scr =='A':
+    print("Excellent")
+elif scr =='B':
+    print("Good")
+elif scr =='C':
+    print("Usually")
+elif scr=='D':
+    print("Effort")
+else:
+    print("Failure")
