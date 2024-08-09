15282d4d88298aa21fc4daf03bb3651e6ea3fe7c
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-08-09 16:07:17 +0900

    Title: 두 사람, Time: 202ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/두 사람/two-person.py
+++ 240809/두 사람/two-person.py
@@ -1,10 +1,10 @@
-info1 = input().split()
-info1_age = int(info1[0])
-info1_gen= info1[1]
+arr = input().split() #input 변수명은 똑같이 써도됨
+info1_age = int(arr[0])
+info1_gen= arr[1]
 
-info2 =input().split()
-info2_age=int(info2[0])
-info2_gen= info2[1]
+arr =input().split()
+info2_age=int(arr[0])
+info2_gen= arr[1]
 
 if (info1_age>=19 and info1_gen=='M') or (info2_age>=19 and info2_gen=='M'):
     print(1)
