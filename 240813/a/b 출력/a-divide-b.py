45d6d066072934f629760cc794fc7a53c1b8d381
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-08-13 16:17:28 +0900

    Title: a/b 출력, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240813/a/b 출력/a-divide-b.py
@@ -0,0 +1,14 @@
+arr=input().split()
+a=int(arr[0])
+b=int(arr[1])
+
+# 정수부분 소수부분 나눠서 처리하면?
+quo = a//b
+rem = a%b
+print(f"{quo}.",end='') #정수
+
+for _ in range(20): #소수 21번째에서 버림이므로 20번째자리까지 나타내줘야함.
+    a*=10
+    quo = a//b
+    a= a%b
+    print(quo,end='')
