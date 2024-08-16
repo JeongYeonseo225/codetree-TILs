6b47697c4e21d0c20c1146e78f8d41f0524b7030
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-08-16 09:09:14 +0900

    Title: 소수 판별기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240816/소수 판별기/prime-number-discriminator.py
+++ 240816/소수 판별기/prime-number-discriminator.py
@@ -1,8 +1,10 @@
 n=int(input())
 sat=False
+
 for i in range(1,n):
-    if n%i!=0 and i!=1:
-        sat=True
+    if i!=1:
+        if n%i!=0:
+            sat=True
 if sat==True:
     print('P')
 else:
