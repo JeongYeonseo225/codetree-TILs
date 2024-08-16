9705ecc99d8fbe8d1dbd10abb98b9d69b4113865
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-08-16 13:51:22 +0900

    Title: 별표 출력하기 12, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240816/별표 출력하기 12/print-star-12.py
+++ 240816/별표 출력하기 12/print-star-12.py
@@ -1,17 +1,21 @@
 n=int(input())
-if n==1:
-    print('*')
-elif n%2==0: #짝수
+#n이 1일땐 1x1
+#n이 짝수일 땐 nxn
+#n이 홀수일 땐 (n-1)xn
+
+if n==1 or n%2==0:
     for i in range(1,n+1):
         for j in range(1, n+1):
-            if i==1 or j==n:
+            if i==1 or j==n or (j%2==0 and i<=j):
                 print('*',end=' ')
             else:
                 print(' ',end=' ')
-else: #홀수
+        print()
+else: 
     for i in range(1, n):
         for j in range(1,n+1):
-            if i==1 and j==n:
+            if i==1 or (j%2==0 and i<=j):
                 print('*',end=' ')
             else:
                 print(' ',end=' ')
+        print()
