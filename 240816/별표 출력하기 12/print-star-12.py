a3c8f76966e43a652ba9edfd0929adb2325c3ff6
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-08-16 13:36:49 +0900

    Title: 별표 출력하기 12, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240816/별표 출력하기 12/print-star-12.py
+++ 240816/별표 출력하기 12/print-star-12.py
@@ -4,7 +4,7 @@ if n==1:
 elif n%2==0: #짝수
     for i in range(1,n+1):
         for j in range(1, n+1):
-            if i==1 or (j%2==0 and j==n):
+            if i==1 or j==n:
                 print('*',end=' ')
             else:
                 print(' ',end=' ')
