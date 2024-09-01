de094848bf718a1966c633d39beff1c0d80b4102
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-09-01 14:33:16 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 150ms, Memory: 35MB, Status: Passed - CodeTree

--- 240901/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240901/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -7,7 +7,9 @@ for j in range(1,n):
     for i in range(n-1,j-1,-1):
         subarr.append(arr[i]-arr[i-j])
 
-if max(subarr)<0:
+if len(subarr)==0:
+    print(0)
+elif max(subarr)<0:
     print(0)
 else:
     print(max(subarr))
