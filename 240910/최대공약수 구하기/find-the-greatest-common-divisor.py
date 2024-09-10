6f4031cd913855a3cd0d26a842588ee4e95defaa
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-09-10 17:10:50 +0900

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240910/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,9 @@
+def print_max_div(n,m):
+    arr=[]
+    for i in range(1,n):
+        if n%i==0 and m%i==0:
+            arr.append(i)
+    print(max(arr))
+
+num1,num2=tuple(map(int,input().split()))
+print_max_div(num1,num2)
