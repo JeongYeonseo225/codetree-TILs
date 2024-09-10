d45f7a5fff0181ea1b994043884a7b71405d1211
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-09-10 17:05:16 +0900

    Title: 숫자로 이루어진 사각형, Time: 222ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240910/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,12 @@
+def print_num(n):
+    cnt=1
+    for i in range(n):
+        for j in range(n):
+            print(cnt,end=' ')
+            cnt+=1
+            if cnt>9:
+                cnt=1
+        print()
+
+num=int(input())
+print_num(num)
