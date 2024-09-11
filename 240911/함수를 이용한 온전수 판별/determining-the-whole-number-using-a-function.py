f4904c25c072a881caf80d909029f534fd405c3d
JeongYeonseo225 <62948547+JeongYeonseo225@users.noreply.github.com>
2024-09-11 15:18:54 +0900

    Title: 함수를 이용한 온전수 판별, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240911/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,15 @@
+def f(n):
+    if n%2==0:
+        return False
+    elif n%10==5:
+        return False
+    elif n%3==0 and n%9!=0:
+        return False
+    return True
+
+a,b=tuple(map(int,input().split()))
+cnt=0
+for i in range(a,b+1):
+    if f(i)==True:
+        cnt+=1
+print(cnt)
